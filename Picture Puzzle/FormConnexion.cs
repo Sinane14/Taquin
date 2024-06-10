69ac7e9c497e0e096f0ad096d99d8d590a4e9483
Sinane14 <sinaneadi@gmail.com>
2024-06-10 09:35:11 +0200

    Update

--- Picture Puzzle/FormConnexion.cs
+++ Picture Puzzle/FormConnexion.cs
@@ -1,63 +1,71 @@
-﻿using System;
-using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-using BCrypt.Net;
+﻿using System; // Importe l'espace de noms System
 
-namespace Picture_Puzzle
+using System.Windows.Forms; // Importe l'espace de noms Windows Forms
+
+using MySql.Data.MySqlClient; // Importe l'espace de noms MySQLClient pour la connexion à MySQL
+
+using BCrypt.Net; // Importe l'espace de noms BCrypt.Net pour le hachage de mot de passe
+
+namespace Picture_Puzzle // Définit un espace de noms appelé Picture_Puzzle
 {
-    public partial class FormConnexion : Form
+    public partial class FormConnexion : Form // Déclare une classe partielle FormConnexion qui hérite de Form
     {
-        public FormConnexion()
+        public FormConnexion() // Définit le constructeur de la classe FormConnexion
         {
-            InitializeComponent();
+            InitializeComponent(); // Initialise le composant du formulaire
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private void btnLogin_Click(object sender, EventArgs e) // Définit l'événement de clic sur le bouton de connexion
         {
-            string connectionString = "server=localhost;database=forum;user=root;password=";
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            string connectionString = "server=localhost;database=forum;user=root;password="; // Définit la chaîne de connexion MySQL
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString)) // Crée une connexion MySQL avec la chaîne de connexion spécifiée
             {
-                try
+                try // Démarre un bloc try-catch pour gérer les exceptions
                 {
-                    conn.Open();
-                    string query = "SELECT Password FROM User WHERE Nickname=@username";
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-                    cmd.Parameters.AddWithValue("@username", txtUsername.Text);
-                    MySqlDataReader reader = cmd.ExecuteReader();
+                    conn.Open(); // Ouvre la connexion à la base de données MySQL
 
-                    if (reader.Read())
+                    string query = "SELECT Password FROM User WHERE Nickname=@username"; // Définit la requête SQL pour récupérer le mot de passe de l'utilisateur
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn); // Crée une nouvelle commande MySQL
+
+                    cmd.Parameters.AddWithValue("@username", txtUsername.Text); // Ajoute un paramètre à la commande pour le nom d'utilisateur
+
+                    MySqlDataReader reader = cmd.ExecuteReader(); // Exécute la commande et lit les données résultantes
+
+                    if (reader.Read()) // Vérifie s'il y a des données lues
                     {
-                        string storedHash = reader.GetString(0);
+                        string storedHash = reader.GetString(0); // Récupère le hachage du mot de passe stocké dans la base de données
 
-                        if (BCrypt.Net.BCrypt.Verify(txtPassword.Text, storedHash))
+                        if (BCrypt.Net.BCrypt.Verify(txtPassword.Text, storedHash)) // Vérifie si le mot de passe saisi correspond au hachage stocké
                         {
-                            MessageBox.Show("Login successful!");
-                            this.Hide();
-                            FormTaquin mainForm = new FormTaquin();
-                            mainForm.FormClosed += (s, args) => this.Close(); // Fermez le formulaire de connexion lorsque le formulaire principal est fermé
-                            mainForm.Show();
+                            MessageBox.Show("Login successful!"); // Affiche un message de succès
+                            this.Hide(); // Cache le formulaire de connexion actuel
+                            FormTaquin mainForm = new FormTaquin(); // Crée une nouvelle instance du formulaire principal du jeu
+                            mainForm.FormClosed += (s, args) => this.Close(); // Ferme le formulaire de connexion lorsque le formulaire principal est fermé
+                            mainForm.Show(); // Affiche le formulaire principal du jeu
                         }
                         else
                         {
-                            MessageBox.Show("Invalid username or password.");
+                            MessageBox.Show("Invalid username or password."); // Affiche un message d'erreur si le nom d'utilisateur ou le mot de passe est incorrect
                         }
                     }
                     else
                     {
-                        MessageBox.Show("User not found.");
+                        MessageBox.Show("User not found."); // Affiche un message d'erreur si l'utilisateur n'est pas trouvé dans la base de données
                     }
                 }
-                catch (Exception ex)
+                catch (Exception ex) // Attrape toute exception
                 {
-                    MessageBox.Show("Error: " + ex.Message);
+                    MessageBox.Show("Error: " + ex.Message); // Affiche un message d'erreur avec le détail de l'exception
                 }
             }
         }
 
-        private void btnid_Click_1(object sender, EventArgs e)
+        private void btnid_Click_1(object sender, EventArgs e) // Définit l'événement de clic sur le bouton d'inscription
         {
-            Connexion1 connexionForm = new Connexion1();
-            connexionForm.Show();
+            Connexion1 connexionForm = new Connexion1(); // Crée une nouvelle instance du formulaire d'inscription
+            connexionForm.Show(); // Affiche le formulaire d'inscription
         }
     }
 }
--- Picture Puzzle/FormTaquin.cs
+++ Picture Puzzle/FormTaquin.cs
@@ -1,34 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Collections;
+﻿using System; // Importe l'espace de noms System
 
-namespace Picture_Puzzle
+using System.Collections.Generic; // Importe l'espace de noms System.Collections.Generic
+
+using System.Drawing; // Importe l'espace de noms System.Drawing pour manipuler des images
+
+using System.Linq; // Importe l'espace de noms System.Linq pour les opérations LINQ
+
+using System.Text; // Importe l'espace de noms System.Text
+
+using System.Threading.Tasks; // Importe l'espace de noms System.Threading.Tasks
+
+using System.Windows.Forms; // Importe l'espace de noms System.Windows.Forms pour les applications Windows Forms
+
+using System.Collections; // Importe l'espace de noms System.Collections pour les collections non génériques
+
+namespace Picture_Puzzle // Définit un espace de noms appelé Picture_Puzzle
 {
-    public partial class FormTaquin : Form
+    public partial class FormTaquin : Form // Déclare une classe partielle FormTaquin qui hérite de Form
     {
         Point EmptyPoint; // Déclare une variable de type Point pour stocker la position vide.
         ArrayList images = new ArrayList(); // Déclare une collection ArrayList pour stocker les images du puzzle.
 
-        public FormTaquin()
+        public FormTaquin() // Définit le constructeur de la classe FormTaquin
         {
             EmptyPoint = new Point(180, 180); // Initialise la position de la case vide
-            InitializeComponent();
+            InitializeComponent(); // Initialise le composant du formulaire
         }
 
-        private void FormTaquin_Load(object sender, EventArgs e)
+        private void FormTaquin_Load(object sender, EventArgs e) // Définit l'événement de chargement du formulaire
         {
             // Méthode vide. Vous pouvez ajouter des initialisations si nécessaire.
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button9_Click(object sender, EventArgs e) // Définit l'événement de clic sur le bouton de démarrage du jeu
         {
-            foreach (Button b in panel1.Controls.OfType<Button>())
-                b.Enabled = true;
+            foreach (Button b in panel1.Controls.OfType<Button>()) // Parcourt tous les boutons dans le panel1
+                b.Enabled = true; // Active tous les boutons
 
             Image original = Image.FromFile(@"img\img.jpg"); // Charge l'image d'origine à partir d'un fichier.
 
@@ -37,49 +44,49 @@ namespace Picture_Puzzle
             AddImagesToButtons(images); // Ajoute les morceaux d'image aux boutons.
         }
 
-        private void AddImagesToButtons(ArrayList images)
+        private void AddImagesToButtons(ArrayList images) // Définit une méthode pour ajouter des images aux boutons
         {
             int i = 0;
             int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
             arr = suffle(arr);
 
-            foreach (Button b in panel1.Controls.OfType<Button>())
+            foreach (Button b in panel1.Controls.OfType<Button>()) // Parcourt tous les boutons dans le panel1
             {
                 if (i < arr.Length)
                 {
-                    b.Image = (Image)images[arr[i]];
+                    b.Image = (Image)images[arr[i]]; // Affecte une image à chaque bouton
                     i++;
                 }
             }
         }
 
-        private int[] suffle(int[] arr)
+        private int[] suffle(int[] arr) // Définit une méthode pour mélanger les éléments d'un tableau
         {
-            Random rand = new Random();
-            return arr.OrderBy(x => rand.Next()).ToArray();
+            Random rand = new Random(); // Initialise un générateur de nombres aléatoires
+            return arr.OrderBy(x => rand.Next()).ToArray(); // Mélange les éléments du tableau et les retourne
         }
 
-        private void cropImageTomages(Image original, int w, int h)
+        private void cropImageTomages(Image original, int w, int h) // Définit une méthode pour découper une image en morceaux
         {
-            Bitmap bmp = new Bitmap(w, h);
+            Bitmap bmp = new Bitmap(w, h); // Crée un nouveau bitmap avec les dimensions spécifiées
 
-            using (Graphics graphic = Graphics.FromImage(bmp))
+            using (Graphics graphic = Graphics.FromImage(bmp)) // Crée une instance Graphics à partir du bitmap
             {
-                graphic.DrawImage(original, 0, 0, w, h);
+                graphic.DrawImage(original, 0, 0, w, h); // Dessine l'image d'origine sur le bitmap
             }
 
             int movr = 0, movd = 0;
 
-            for (int x = 0; x < 8; x++)
+            for (int x = 0; x < 8; x++) // Parcourt les morceaux de l'image
             {
-                Bitmap piece = new Bitmap(90, 90);
+                Bitmap piece = new Bitmap(90, 90); // Crée un nouveau bitmap pour chaque morceau
 
-                for (int i = 0; i < 90; i++)
-                    for (int j = 0; j < 90; j++)
-                        piece.SetPixel(i, j, bmp.GetPixel(i + movr, j + movd));
+                for (int i = 0; i < 90; i++) // Parcourt les pixels horizontalement
+                    for (int j = 0; j < 90; j++) // Parcourt les pixels verticalement
+                        piece.SetPixel(i, j, bmp.GetPixel(i + movr, j + movd)); // Copie le pixel de l'image d'origine dans le morceau
 
-                images.Add(piece);
+                images.Add(piece); // Ajoute le morceau à la collection d'images
 
                 movr += 90;
 
@@ -91,39 +98,39 @@ namespace Picture_Puzzle
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // Définit l'événement de clic sur un bouton du puzzle
         {
-            MoveButton((Button)sender);
+            MoveButton((Button)sender); // Appelle la méthode pour déplacer le bouton
         }
 
-        private void MoveButton(Button btn)
+        private void MoveButton(Button btn) // Définit une méthode pour déplacer un bouton
         {
             if (((btn.Location.X == EmptyPoint.X - 90 || btn.Location.X == EmptyPoint.X + 90) && btn.Location.Y == EmptyPoint.Y) ||
                 (btn.Location.Y == EmptyPoint.Y - 90 || btn.Location.Y == EmptyPoint.Y + 90) && btn.Location.X == EmptyPoint.X)
             {
-                Point swap = btn.Location;
-                btn.Location = EmptyPoint;
-                EmptyPoint = swap;
+                Point swap = btn.Location; // Stocke la position du bouton dans une variable temporaire
+                btn.Location = EmptyPoint; // Déplace le bouton à la position vide
+                EmptyPoint = swap; // Met à jour la position vide avec la position précédente du bouton
             }
 
-            if (EmptyPoint.X == 180 && EmptyPoint.Y == 180)
-                CheckValid();
+            if (EmptyPoint.X == 180 && EmptyPoint.Y == 180) // Vérifie si le puzzle est résolu
+                CheckValid(); // Vérifie si le puzzle est valide
         }
 
-        private void CheckValid()
+        private void CheckValid() // Définit une méthode pour vérifier si le puzzle est résolu
         {
             int count = 0, index;
 
-            foreach (Button btn in panel1.Controls.OfType<Button>())
+            foreach (Button btn in panel1.Controls.OfType<Button>()) // Parcourt tous les boutons dans le panel1
             {
-                index = (btn.Location.Y / 90) * 3 + btn.Location.X / 90;
+                index = (btn.Location.Y / 90) * 3 + btn.Location.X / 90; // Calcule l'index de l'image correspondante
 
-                if (images[index] == btn.Image)
-                    count++;
+                if (images[index] == btn.Image) // Vérifie si l'image du bouton correspond à l'image attendue
+                    count++; // Incrémente le compteur
             }
 
-            if (count == 8)
-                MessageBox.Show("Vous avez gagné !");
+            if (count == 8) // Vérifie si toutes les images sont à leur place
+                MessageBox.Show("Vous avez gagné !"); // Affiche un message de victoire
         }
     }
 }
